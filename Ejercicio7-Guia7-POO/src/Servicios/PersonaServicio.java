7e1fd8216fdd05ec2516f77ed309f736dcc85b62
Javier Deslarmes <javierdeslarmes@yahoo.com.ar>
2022-08-24 23:39:43 -0300

    Sexto Commit Ejercicio 7 Guia 7 POO

--- Ejercicio7-Guia7-POO/src/Servicios/PersonaServicio.java
+++ Ejercicio7-Guia7-POO/src/Servicios/PersonaServicio.java
@@ -5,10 +5,64 @@
  */
 package Servicios;
 
+import Entidades.Persona;
+import java.util.Scanner;
+
 /**
  *
  * @author jdeslarmes
  */
 public class PersonaServicio {
+    
+    Scanner leer = new Scanner (System.in);
+    
+    //Método crear persona
+    
+    public Persona crearPersona(){
+        
+        Persona p1 = new Persona();
+        
+        System.out.println("Ingrese el nombre: ");
+        p1.setNombre(leer.next());
+        System.out.println("Ingrese la edad: ");
+        p1.setEdad(leer.nextInt());
+        System.out.println("Ingrese el sexo (M)/(F)/(O): ");
+        p1.setSexo(leer.next());
+        while (!(p1.getSexo().equals("M") || p1.getSexo().equals("F") || p1.getSexo().equals("O") )){
+            System.out.println("Ingrese nuevamente el sexo (M)/(F)/(O): ");
+           p1.setSexo(leer.nextLine());
+        } 
+        System.out.println("Ingrese el peso en Kg sin decimales");
+        p1.setPeso(leer.nextInt());
+        System.out.println("Ingrese la altura en mts");
+        p1.setAltura(leer.nextDouble());
+
+        return p1;         
+        
+    }
+    
+    public int calcularMC(Persona p1){
+    
+
+    Double resultado = p1.getPeso()/(p1.getAltura()*p1.getAltura());
+    
+    if (resultado < 20.0){
+        
+        return -1;
+    } if (resultado < 25.0 & resultado > 20.0){
+
+        return 0;
+    }else
+            
+        return 1;
+    }
+    
+    public Boolean esMayorDeEdad(Persona p1){
+    
+        if(p1.getEdad() > 17){
+        return true;
+        } else
+        return false;
+    }
     
 }
--- Ejercicio7-Guia7-POO/src/ejercicio7/guia7/poo/Ejercicio7Guia7POO.java
+++ Ejercicio7-Guia7-POO/src/ejercicio7/guia7/poo/Ejercicio7Guia7POO.java
@@ -5,6 +5,10 @@
  */
 package ejercicio7.guia7.poo;
 
+import Entidades.Persona;
+import Servicios.PersonaServicio;
+import java.util.Scanner;
+
 /**
  *
  * @author jdeslarmes
@@ -15,7 +19,108 @@ public class Ejercicio7Guia7POO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+             Scanner leer = new Scanner(System.in);
+     
+     Double sobrepeso = 0.0;
+     Double ideal = 0.0;
+     Double menospeso = 0.0; 
+     Double menor = 0.0;
+     Double mayor =0.0;
+                
+             
+             //Instaciar un objeto de tipo Servicio
+             
+             PersonaServicio ps1 = new PersonaServicio();
+
+             
+             //Intanciación del objeto donde alojar el retorno de datos
+             
+             Persona p1 = ps1.crearPersona();
+             
+             //Calculo si esta dentro del peso ideal la persona 1
+             if(ps1.calcularMC(p1) == -1){
+                menospeso += 1 ;
+               }if (ps1.calcularMC(p1) == 0){
+                ideal += 1 ;
+               }if (ps1.calcularMC(p1) == 1){   
+                sobrepeso += 1 ;
+             }   
+             //Calculo si es mayor de edad la persona 1  
+             if(ps1.esMayorDeEdad(p1)){
+                 mayor += 1;
+             } else {
+                 menor += 1;
+             }
+             
+            Persona p2 = ps1.crearPersona();
+
+              //Calculo si esta dentro del peso ideal la persona 2
+            if(ps1.calcularMC(p2) == -1){
+                menospeso += 1 ;
+               }if (ps1.calcularMC(p2) == 0){
+                ideal += 1 ;
+               } if (ps1.calcularMC(p2) == 1){   
+                sobrepeso += 1 ;
+               }     
+               //Calculo si es mayor de edad la persona 2
+              if(ps1.esMayorDeEdad(p2)){
+                 mayor += 1;
+             } else {
+                 menor += 1;
+             }
+                     
+
+             Persona p3 = ps1.crearPersona();
+             
+             //Calculo si esta dentro del peso ideal la persona 3
+              if(ps1.calcularMC(p3) == -1){
+                menospeso += 1 ;
+               }if (ps1.calcularMC(p3) == 0){
+                ideal += 1 ;
+               } if (ps1.calcularMC(p3) == 1){   
+                sobrepeso += 1 ;
+             }        
+             
+             //Calculo si es mayor de edad la persona 3
+              if(ps1.esMayorDeEdad(p3)){
+                 mayor += 1;
+             } else {
+                 menor += 1;
+             }
+             
+             Persona p4 = ps1.crearPersona();
+
+             //Calculo si esta dentro del peso ideal la persona 4
+             if(ps1.calcularMC(p4) == -1){
+                menospeso += 1 ;
+               }if (ps1.calcularMC(p4) == 0){
+                ideal += 1 ;
+               } if (ps1.calcularMC(p4) == 1){   
+                sobrepeso += 1 ;
+             } 
+            //Calculo si es mayor de edad la persona 4
+              if(ps1.esMayorDeEdad(p4)){
+                 mayor += 1;
+             } else {
+                 menor += 1;
+             }
+            
+            Double menorPeso = ( menospeso / 4 )*100;
+            Double idealPeso = ( ideal / 4 )*100;
+            Double sobrePeso = ( sobrepeso / 4 )*100;
+            Double mayorEdad = ( mayor / 4 )*100;
+            Double menorEdad = ( menor / 4 )*100;
+     
+              
+            System.out.println("El porcentaje de personas con menor peso del ideal es: % " + menorPeso  );
+            System.out.println("El porcentaje de personas con peso ideal es: % " +  idealPeso );
+            System.out.println("El porcentaje de personas con sobrepeso es: % " +  sobrePeso );
+            System.out.println("El porcentaje de personas mayores es: % " +  mayorEdad );
+            System.out.println("El porcentaje de personas menores es: % " +  (menor /4)*100 );
+     
+     
 }
+     
+}     
+     
+     
