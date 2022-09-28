c10bb255ecc3187e5f19260027b57644137d4027
Javier Deslarmes <javierdeslarmes@yahoo.com.ar>
2022-09-27 21:38:51 -0300

    Agregar final 2 ejercicio 3 de Guia 8 Colecciones

---
+++ Ejercicio3-Guia8-Colecciones/src/ejercicio3/guia8/colecciones/Ejercicio3Guia8Colecciones.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3.guia8.colecciones;
+
+import servicios.ServicioAlumno;
+
+/**
+ *
+ * @author jdeslarmes
+ */
+public class Ejercicio3Guia8Colecciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    
+        ServicioAlumno sa = new ServicioAlumno();
+     
+       sa.crearAlumno();
+       sa.agregarAlumno();
+      //sa.mostrarListaAlumnos();
+       sa.mostrarLista2Alumnos();
+
+        
+    }
+    
+}
